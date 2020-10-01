3971302170016d32b6fe1e59b0d63b734ea298e0
Thanh Hung <hungpt.se@gmail.com>
2020-10-01 17:56:16 +0700

    user and post

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,20 +3,20 @@ import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService) {
+    this.httpService.axiosRef.interceptors.request.use(config => {
+      config.headers.Authorization = 'Bearer AAAAAAAAAAAAAAAAAAAAAP6DHwEAAAAABD%2Bf%2F9Nsz%2F9YQimL9XpubnOe7M0%3DSmxxdZ6HjmR6reMjTo7tabCODLpda3B0eDCCVJE1hEEK26MZHZ';
+      return config;
+    });
+  }
   getHello(): string {
     return 'Hello World!';
   }
   API_TWITTER_V1 = 'https://api.twitter.com/1.1';
   API_TWITTER_V2 = 'https://api.twitter.com/2';
 
-  @Cron(new Date(Date.now() + 1000))
-  async crawlUserInfo() {
-    this.httpService.get(`${this.API_TWITTER_V1}/users/lookup.json?screen_name=${"realDonaldTrump"}`, {
-      headers: {
-        'Authorization' : 'Bearer AAAAAAAAAAAAAAAAAAAAAP6DHwEAAAAABD%2Bf%2F9Nsz%2F9YQimL9XpubnOe7M0%3DSmxxdZ6HjmR6reMjTo7tabCODLpda3B0eDCCVJE1hEEK26MZHZ'
-      }
-    }).subscribe(res => {
+  async crawlUserInfo(username: string) {
+    this.httpService.get(`${this.API_TWITTER_V1}/users/lookup.json?screen_name=${username}`).subscribe(res => {
       if (res.data !== []) {
         const info = res.data[0];
         console.log('id', info.id);
@@ -28,17 +28,12 @@ export class AppService {
         console.log('isVerify', info.verified);
         console.log('geo_enabled', info.geo_enabled);
       }
-
     });
   }
 
-  @Cron(new Date(Date.now() + 2000))
-  async crawlPost() {
-    this.httpService.get(`${this.API_TWITTER_V2}/tweets/search/recent?query=from:${"realDonaldTrump"}`, {
-      headers: {
-        'Authorization' : 'Bearer AAAAAAAAAAAAAAAAAAAAAP6DHwEAAAAABD%2Bf%2F9Nsz%2F9YQimL9XpubnOe7M0%3DSmxxdZ6HjmR6reMjTo7tabCODLpda3B0eDCCVJE1hEEK26MZHZ'
-      }
-    }).subscribe(res => {
+ 
+  async crawlPost(username: string) {
+    this.httpService.get(`${this.API_TWITTER_V2}/tweets/search/recent?query=from:${username}`).subscribe(res => {
       if (res.data !== []) {
         res.data.data.forEach(item => {
           console.log(item.id, item.text);
@@ -46,6 +41,13 @@ export class AppService {
       }
 
     });
+  }
+
+  @Cron(new Date(Date.now() + 500))
+  async crawlUserAndPost(){
+     const userInfo = await this.crawlUserInfo('realDonaldTrump');
+     const userPost = await this.crawlPost('realDonaldTrump');
+     
   }
 
 }
